d94865d7601c96f0b48dcc0ed0eda0ae50a7bdcc
John Caridi <John.Caridi@openlink.com>
2018-12-13 13:54:55 -0500

    Started adding connection to LogInService

--- MusicCollectionServices/LogInService.cs
+++ MusicCollectionServices/LogInService.cs
@@ -1,7 +1,57 @@
 ﻿using MusicCollectionModel.Interfaces;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace MusicCollectionServices
 {
+    public class DbConnection
+    {
+        private DbConnection()
+        {
+        }
+
+        private readonly string _databaseHost = "johncaridi.com";
+        private readonly string _databaseName = "johnca14_music_collection";
+        private readonly string _userName = "johnca14_music_collection_session";
+        private readonly string _password = "Y%ELM8;p?_=6";
+        private readonly string _port = "3306";
+
+
+        private MySqlConnection _connection = null;
+        public MySqlConnection Connection
+        {
+            get { return _connection; }
+        }
+
+        private static DbConnection _instance = null;
+        public static DbConnection Instance()
+        {
+            if (_instance == null)
+                _instance = new DbConnection();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            if (Connection == null)
+            {
+                if (string.IsNullOrEmpty(_databaseName))
+                    return false;
+                string connstring = $"Server=\"{_databaseHost}\"; database=\"{_databaseName}\"; UID=\"{_userName}\"; Port=\"{_port}\"; password=\"{_password}\"";
+                _connection = new MySqlConnection(connstring);
+                _connection.Open();
+            }
+
+            return true;
+        }
+
+        public void Close()
+        {
+            _connection.Close();
+            _connection = null;
+        }
+    }
+
     public class LogInService : ILogInService
     {
         private class UserInfo : IUserInfo
@@ -12,9 +62,25 @@ namespace MusicCollectionServices
         }
         public IUserInfo LogIn(string userName, string password)
         {
-            if (userName.Equals("test") && password.Equals("test"))
+            var dbCon = DbConnection.Instance();
+            if (dbCon.IsConnect())
             {
-                return new UserInfo { UserName = userName, LibraryName = "TEST_LIBRARY" };
+                string query = "SELECT * FROM music_collection_users";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string userUserName = reader.GetString(0);
+                    string userPassword = reader.GetString(1);
+
+                    if (userName.Equals(userUserName) && password.Equals(userPassword))
+                    {
+                        return new UserInfo { UserName = userName, LibraryName = "TEST_LIBRARY" };
+                    }
+                }
+
+                dbCon.Close();
             }
 
             return null;
